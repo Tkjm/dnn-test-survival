37cae5c1e8f8f628b5998989dfe4814d3d7ba804
Tmjk <thomasandyjk@gmail.com>
2019-04-16 22:42:31 +0800

    Update nntest to start using the environment

--- nntest.py
+++ nntest.py
@@ -1,14 +1,23 @@
 from environment import Environment
-import tensorflow as tf
+# import tensorflow as tf
 import numpy as np
 import pdb
 
 env = Environment()
-env.step(0)
 
-model = tf.keras.models.Sequential([
-  tf.keras.layers.Flatten(input_shape=(8, 8)),
-  tf.keras.layers.Dense(64, activation=tf.nn.relu),
-  tf.keras.layers.Dropout(0.2),
-  tf.keras.layers.Dense(4, activation=tf.nn.softmax)
-])
+for i_episode in range(20):
+    observation = env.reset()
+    print("Episode Start")
+    for t in range(100):
+        print(observation)
+        observation, reward, done = env.step(np.random.randint(0, 3))
+        if done:
+            print("Episode finished after {} timesteps".format(t+1))
+            break
+
+# model = tf.keras.models.Sequential([
+#   tf.keras.layers.Flatten(input_shape=(8, 8)),
+#   tf.keras.layers.Dense(64, activation=tf.nn.relu),
+#   tf.keras.layers.Dropout(0.2),
+#   tf.keras.layers.Dense(4, activation=tf.nn.softmax)
+# ])
