8a1c848d57e1b59ce4cdf921f8d54b714516ecfe
Tmjk <thomasandyjk@gmail.com>
2019-04-16 22:41:01 +0800

    Update environment to a very basic one.

--- environment.py
+++ environment.py
@@ -1,10 +1,30 @@
 import numpy as np
 import pdb
+from typing import Tuple
 
 
 class Environment():
     def __init__(self) -> None:
         self.map = np.ndarray(shape=(8, 8))
+        self.rewardPos = 63
+        self.reset()
 
-    def step(self, action: int) -> None:
-        pass
+    def step(self, action: int) -> Tuple[int, float, bool]:
+        if action == 0 and self.agentPos // 8 > 0:
+            self.agentPos -= 8
+        elif action == 1 and self.agentPos % 8 < 7:
+            self.agentPos += 1
+        elif action == 2 and self.agentPos // 8 < 7:
+            self.agentPos += 8
+        elif action == 3 and self.agentPos % 8 > 0:
+            self.agentPos -= 1
+        reward = 0.0
+        if self.agentPos == self.rewardPos:
+            reward = 1.0
+            self.episodeOver = True
+        return self.agentPos, reward, self.episodeOver
+
+    def reset(self) -> int:
+        self.agentPos = 0
+        self.episodeOver = False
+        return self.agentPos
