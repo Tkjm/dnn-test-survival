c0299222486cc12664d48deaed1aca952aa7f6d7
Tmjk <thomasandyjk@gmail.com>
2019-04-17 20:54:00 +0800

    Update nntest to use a very basic q-learning algorithm

--- environment.py
+++ environment.py
@@ -1,30 +1,36 @@
 import numpy as np
 import pdb
 from typing import Tuple
+from gym.spaces import Discrete
 
 
 class Environment():
     def __init__(self) -> None:
         self.map = np.ndarray(shape=(8, 8))
-        self.rewardPos = 63
+        self.reward_pos = np.array([7, 7])
+        self.action_space = Discrete(4)
+        self.observation_space = Discrete(64)
         self.reset()
 
     def step(self, action: int) -> Tuple[int, float, bool]:
-        if action == 0 and self.agentPos // 8 > 0:
-            self.agentPos -= 8
-        elif action == 1 and self.agentPos % 8 < 7:
-            self.agentPos += 1
-        elif action == 2 and self.agentPos // 8 < 7:
-            self.agentPos += 8
-        elif action == 3 and self.agentPos % 8 > 0:
-            self.agentPos -= 1
+        if action == 0 and self.agent_pos[0] > 0:
+            self.agent_pos[0] -= 1
+        elif action == 1 and self.agent_pos[1] < 7:
+            self.agent_pos[1] += 1
+        elif action == 2 and self.agent_pos[0] < 7:
+            self.agent_pos[0] += 1
+        elif action == 3 and self.agent_pos[1] > 0:
+            self.agent_pos[1] -= 1
         reward = 0.0
-        if self.agentPos == self.rewardPos:
+        if np.array_equal(self.agent_pos, self.reward_pos):
             reward = 1.0
-            self.episodeOver = True
-        return self.agentPos, reward, self.episodeOver
+            self.episode_over = True
+        return self.__get_observation(), reward, self.episode_over
 
     def reset(self) -> int:
-        self.agentPos = 0
-        self.episodeOver = False
-        return self.agentPos
+        self.agent_pos = np.array([0, 0])
+        self.episode_over = False
+        return self.__get_observation()
+
+    def __get_observation(self) -> int:
+        return self.agent_pos[0] * 8 + self.agent_pos[1]
--- nntest.py
+++ nntest.py
@@ -1,19 +1,51 @@
 from environment import Environment
-# import tensorflow as tf
 import numpy as np
+# import tensorflow as tf
 import pdb
 
-env = Environment()
 
-for i_episode in range(20):
+learning_rate = 0.1
+discount = 0.95
+exploration_rate = 1.0
+exploration_delta = 1.0 / 200
+
+env = Environment()
+q_table = np.zeros((env.observation_space.n, env.action_space.n))
+
+np.set_printoptions(precision=4)
+
+for episode in range(200):
     observation = env.reset()
-    print("Episode Start")
-    for t in range(100):
-        print(observation)
-        observation, reward, done = env.step(np.random.randint(0, 3))
+    print("Episode {} starts:".format(episode))
+    done = False
+    for step in range(200):
+        action = None
+        if np.random.random() > exploration_rate:
+            action = np.random.choice(
+                np.flatnonzero(
+                    q_table[observation] == q_table[observation].max()
+                )
+            )
+        else:
+            action = env.action_space.sample()
+        new_ob, reward, done = env.step(action)
+        print('{:>4} {:>4} {}'.format(new_ob, reward, q_table[new_ob]))
+        future_reward = q_table[new_ob][np.random.choice(
+            np.flatnonzero(
+                q_table[new_ob] == q_table[new_ob].max()
+            )
+        )]
+        q_table[observation][action] += learning_rate\
+            * (reward + discount * future_reward
+               - q_table[observation][action])
+        if exploration_rate > 0.0:
+            exploration_rate -= exploration_delta
+        observation = new_ob
         if done:
-            print("Episode finished after {} timesteps".format(t+1))
+            print("Episode finished after {} timesteps".format(step+1))
             break
+    if not done:
+        print("Episode timed out after {} timesteps".format(200))
 
 # model = tf.keras.models.Sequential([
 #   tf.keras.layers.Flatten(input_shape=(8, 8)),
