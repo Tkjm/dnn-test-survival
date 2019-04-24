ac49b0a066122498a56c67866529f4608cfcdc78
Tmjk <thomasandyjk@gmail.com>
2019-04-25 03:42:07 +0800

    Update puzzle2048
    Now it's like a real 2048.

--- puzzle2048/puzzle2048_env.py
+++ puzzle2048/puzzle2048_env.py
@@ -2,6 +2,7 @@ import gym
 from gym.spaces import Discrete, Box
 import numpy as np
 from typing import Tuple
+from pdb import set_trace
 
 
 class Puzzle2048_env(gym.Env):
@@ -10,34 +11,51 @@ class Puzzle2048_env(gym.Env):
     def __init__(self) -> None:
         self.width = 4
         self.height = 4
-        self.map = np.zeros(shape=(self.height, self.width))
-        self.reward_pos = np.array([self.height - 1, self.width - 1])
-        self.map[tuple(self.reward_pos)] = 1.0
         self.action_space = Discrete(4)
         self.observation_space = Box(0.0, 1.0, (self.height, self.width))
         self.reset()
 
     def step(self, action: int) -> Tuple[np.ndarray, float, bool]:
-        if action == 0 and self.agent_pos[0] > 0:
-            self.agent_pos[0] -= 1
-        elif action == 1 and self.agent_pos[1] < self.width - 1:
-            self.agent_pos[1] += 1
-        elif action == 2 and self.agent_pos[0] < self.height - 1:
-            self.agent_pos[0] += 1
-        elif action == 3 and self.agent_pos[1] > 0:
-            self.agent_pos[1] -= 1
+        temp_map = np.rot90(self.map, action)
         reward = 0.0
-        if np.array_equal(self.agent_pos, self.reward_pos):
-            reward = 1.0
-            self.episode_over = True
+        for col in range(self.width):
+            row_merge = 0
+            for row in range(1, self.height):
+                if temp_map[row, col] == 0.0:
+                    continue
+                if temp_map[row, col] == temp_map[row_merge, col]:
+                    temp_map[row_merge, col] += 1.0
+                    if temp_map[row_merge, col] == 8:
+                        self.episode_over = True
+                    temp_map[row, col] = 0.0
+                    reward += temp_map[row_merge, col]
+                else:
+                    if temp_map[row_merge, col] != 0.0:
+                        row_merge += 1
+                    temp_map[row_merge, col], temp_map[row, col] =\
+                        temp_map[row, col], temp_map[row_merge, col]
+        self.map = np.rot90(temp_map, 4 - action)
         self.step_count += 1
+        self.add_tile()
+        zero_indices = np.argwhere(self.map == 0)
+        if zero_indices.size == 0\
+            and 0.0 not in np.diff(self.map)\
+                and 0.0 not in np.diff(self.map.T):
+            self.episode_over = True
         return self.__get_observation(), reward, self.episode_over
 
     def reset(self) -> np.ndarray:
-        self.agent_pos = np.array([0, 0])
+        self.map = np.zeros(shape=(self.height, self.width))
         self.episode_over = False
         self.step_count = 0
+        self.add_tile()
         return self.__get_observation()
+
+    def add_tile(self) -> None:
+        zero_indices = np.argwhere(self.map == 0)
+        if zero_indices.size > 0:
+            choice = zero_indices[np.random.choice(zero_indices.shape[0])]
+            self.map[tuple(choice)] = np.random.randint(1, 3)
 
     def render(self, mode='rgb_array'):
         if mode == 'rgb_array':
@@ -49,5 +67,4 @@ class Puzzle2048_env(gym.Env):
 
     def __get_observation(self) -> np.ndarray:
         observation = np.copy(self.map)
-        observation[tuple(self.agent_pos)] = -1.0
         return observation
--- train.py
+++ train.py
@@ -9,22 +9,25 @@ def train_episode(env, agent, maximum_steps: int,
     Train an episode and return:
         timed_out (bool): whether the training hasn't end before maximum_steps
         step_count (int): the step count when the episode end
+        total_reward (int): the accumulated reward when the episode end
     '''
     observation = env.reset()
+    total_reward = 0.0
     for step in range(maximum_steps):
         action = agent.get_action(observation)
         new_ob, reward, done = env.step(action)
-        outputFunc('{:>2}; {} -> {}; r = {:>3}'.format(
+        outputFunc('{:>2}\n{} ->\n{}; r = {:>5}'.format(
             action,
             observation,
             new_ob,
             reward,
         ))
+        total_reward += reward
         agent.update(observation, new_ob, action, reward)
         observation = new_ob
         if done:
-            return False, step + 1
-    return not True, maximum_steps
+            return False, step + 1, total_reward
+    return not True, maximum_steps, total_reward
 
 
 def train(env, agent, episode_count: int, maximum_steps: int,
@@ -34,12 +37,13 @@ def train(env, agent, episode_count: int, maximum_steps: int,
 
     np.set_printoptions(precision=6, suppress=True)
     for episode in range(episode_count):
-        timed_out, step_count = train_episode(
+        timed_out, step_count, total_reward = train_episode(
             env, agent, maximum_steps, outputFunc)
         outputFunc("Episode {} {} after step {}.".format(
             episode + 1,
             "timed out" if timed_out else "end",
             step_count,
         ))
+        outputFunc("Total Reward: {:>5}".format(total_reward))
         agent.next_episode()
     np.set_printoptions()
